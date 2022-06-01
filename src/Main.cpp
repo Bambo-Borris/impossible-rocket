926d468fbf9b80a3708683c7c064b3f0bef2b7f6
Bambo-Borris <canyoufeelit4@gmail.com>
2022-06-01 19:12:25 +0100

    Induce compile error with spdlog

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,18 @@
 #include "App.hpp"
 
 #include <SFML/GpuPreference.hpp>
-
+#include <spdlog/spdlog.h> // JUST TO REPRO COMPILE ERROR, REMOVE AFTER FIX
 SFML_DEFINE_DISCRETE_GPU_PREFERENCE
 
 int main(int argc, char *argv[])
 {
 	(void)(argc);
 	(void)(argv);
+
+	// SPDLOG WILL BUILD FINE WITH THIS LINE SINCE 0 ARGS
+	spdlog::debug("Not broken");
+	// SPDLOG WILL CAUSE A COMPILE ERROR WITH THIS LINE
+	spdlog::debug("Broken {}", 2.0f);
 
 	App app;
 	app.run();
