bf4ff0ece1cd03f4a59484be0384b7db319df686
Bambo-Borris <canyoufeelit4@gmail.com>
2022-06-09 12:23:46 +0100

    Pause menu bound to P/EScape keys with sfx

--- src/PlayState.hpp
+++ src/PlayState.hpp
@@ -20,21 +20,38 @@ public:
 	virtual void draw() const override;
 
 private:
-	void particleEffectUpdate(); 
+	enum class Status
+	{
+		Playing,
+		Paused,
+		End_Of_Level
+	};
+
+	void updatePlaying(const sf::Time &dt);
+	void updatePaused(const sf::Time &dt);
+	void updateEndOfLevel(const sf::Time &dt);
+	void particleEffectUpdate();
 	void outOfBoundsUpdate();
-	
+
 	PhysicsWorld m_physicsWorld;
 	GameLevel m_gameLevel;
 	PlayerRocket m_rocket;
 
 	sf::RectangleShape m_backgroundSprite;
-	sf::RectangleShape m_oobDirectionIndicator; 
+	sf::RectangleShape m_oobDirectionIndicator;
+	sf::RectangleShape m_pauseMenuDim;
 
 	sf::Text m_uiPadType;
 	sf::Text m_uiAttempts;
 	sf::Text m_uiOOB;
+	sf::Text m_uiPauseTitle;
+	sf::Text m_uiResumeButton;
+	sf::Text m_uiOptionsButton;
+	sf::Text m_uiQuitButton;
 	sf::Clock m_oobTimer; // out of bounds timer
+	sf::Sound m_pauseButtonHoverSfx;
 
 	std::vector<std::unique_ptr<ParticleEffect>> m_particleEffects;
 	bool m_isOutOfBounds{false};
+	PlayState::Status m_status{PlayState::Status::Playing};
 };
